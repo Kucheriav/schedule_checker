74a39e839036b99b042aa7f6c9640c8771924d17
Kucheriav <kuchborisovich@gmail.com>
2024-09-17 22:54:25 +0300

    implemented checking difference with files of different number of classes. added some widgets. need to think about progress bars. need to implemenet assembling certain day schedule. started to rewrite sche_che into Class

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 from design import Ui_MainWindow
 from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QLabel
 from openpyxl import load_workbook, Workbook
-from sche_che import row_normalization, bold_difference
+from sche_che import Sche_Che
 import sys
-
-G = 9.8
-STEP_TIME = 1
-GRID_STEP = 25
 
 
 class Window(QMainWindow, Ui_MainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
         self.setupUi(self)
+        self.comboBox.setEnabled(False)
+        self.comboBox.addItems(['понедельник', 'вторник', 'среда', 'четверг', 'пятница'])
         self.openBaseScheduleBtn.clicked.connect(self.openFile)
         self.openNewScheduleBtn.clicked.connect(self.openFile)
         self.findChangesBtn.clicked.connect(self.compare_files)
+        self.checkBox.clicked.connect(lambda x: self.comboBox.setEnabled(not self.comboBox.isEnabled()))
         self.work_dir = ''
         self.base_schedule = None
         self.new_schedule = None
         self.findChangesBtn.setEnabled(False)
+        self.schedule_checker = Sche_Che()
 
 
     def openFile(self):
@@ -39,7 +39,7 @@ class Window(QMainWindow, Ui_MainWindow):
 
         self.base_schedule = row_normalization(self.base_schedule)
         self.new_schedule = row_normalization(self.new_schedule)
-        bold_difference(self.base_schedule, self.new_schedule)
+        bold_difference_v2(self.base_schedule, self.new_schedule)
         self.new_schedule.save(f"{self.work_dir}/{self.newScheduleLabel.text().split('/')[1]}_checked.xlsx")
 
 
--- sche_che.py
+++ sche_che.py
@@ -1,101 +1,110 @@
 from openpyxl import load_workbook, Workbook
 from openpyxl.styles import Font
+from PyQt5.QtCore import QObject, pyqtSignal
 import argparse
 from tqdm import tqdm
 
+class Sche_Che(QObject):
+    progressChanged = pyqtSignal(int)
 
-def row_normalization(wb):
-    # будем приводить в единый вид все файлы
-    # просто делаю каждую строку урока сдвоенной
-    # для простоты будем задваивать все строки.
+    def row_normalization(self, wb):
+        # будем приводить в единый вид все файлы
+        # просто делаю каждую строку урока сдвоенной
+        # для простоты будем задваивать все строки.
 
-    ws = wb.active
-    wb_out = Workbook()
-    ws_out = wb_out.active
+        ws = wb.active
+        wb_out = Workbook()
+        ws_out = wb_out.active
 
-    rows_with_merged_cells = set()
-    for merge_range in ws.merged_cells.ranges:
-        if merge_range.min_row == merge_range.max_row - 1 and merge_range.min_col == merge_range.max_col:
-            rows_with_merged_cells.add(merge_range.min_row)
+        rows_with_merged_cells = set()
+        for merge_range in ws.merged_cells.ranges:
+            if merge_range.min_row == merge_range.max_row - 1 and merge_range.min_col == merge_range.max_col:
+                rows_with_merged_cells.add(merge_range.min_row)
 
-    row = 1
-    total_rows = ws.max_row - len(rows_with_merged_cells)
-    pbar = tqdm(total=total_rows)
-    while row < ws.max_row:
-        if not row in rows_with_merged_cells:
-            ws_out.append([cell.value for cell in ws[row]])
-            ws_out.append([None for cell in ws[row]])
-            for col in range(1, len(ws[row]) + 1):
-                ws_out.merge_cells(start_row=ws_out.max_row - 1, start_column=col, end_row=ws_out.max_row,
-                                   end_column=col)
-            # есть непрокрасы. не стал разбираться
-            # for col in range(1, len(ws[row]) + 1):
-            #     cell_in = ws.cell(row=row, column=col)
-            #     cell_out = ws_out.cell(row=ws_out.max_row - 1, column=col)
-            #     cell_out.value = cell_in.value
-            #     cell_out.font = copy(cell_in.font)
-            #     cell_out.fill = copy(cell_in.fill)
-            #     cell_out.border = copy(cell_in.border)
-            row += 1
-        else:
-            ws_out.append([cell.value for cell in ws[row]])
-            row += 1
-            ws_out.append([cell.value for cell in ws[row]])
-            for col in range(1, len(ws[row]) + 1):
-                if ws_out.cell(ws_out.max_row, col).value is None:
+        row = 1
+        total_rows = ws.max_row - len(rows_with_merged_cells)
+        pbar = tqdm(total=total_rows)
+        while row < ws.max_row:
+            if not row in rows_with_merged_cells:
+                ws_out.append([cell.value for cell in ws[row]])
+                ws_out.append([None for cell in ws[row]])
+                for col in range(1, len(ws[row]) + 1):
                     ws_out.merge_cells(start_row=ws_out.max_row - 1, start_column=col, end_row=ws_out.max_row,
                                        end_column=col)
+                # есть непрокрасы. не стал разбираться
+                # for col in range(1, len(ws[row]) + 1):
+                #     cell_in = ws.cell(row=row, column=col)
+                #     cell_out = ws_out.cell(row=ws_out.max_row - 1, column=col)
+                #     cell_out.value = cell_in.value
+                #     cell_out.font = copy(cell_in.font)
+                #     cell_out.fill = copy(cell_in.fill)
+                #     cell_out.border = copy(cell_in.border)
+                row += 1
+            else:
+                ws_out.append([cell.value for cell in ws[row]])
+                row += 1
+                ws_out.append([cell.value for cell in ws[row]])
+                for col in range(1, len(ws[row]) + 1):
+                    if ws_out.cell(ws_out.max_row, col).value is None:
+                        ws_out.merge_cells(start_row=ws_out.max_row - 1, start_column=col, end_row=ws_out.max_row,
+                                           end_column=col)
+                row += 1
+            pbar.update(1)
+        pbar.close()
+        # постобработка
+        # мешаем эксельному движку опять все превратить в дата/время
+        for row in range(1, ws_out.max_row + 1):
+            if '#' in str(ws_out.cell(row, 1).value):
+                for col in range(1,6):
+                    ws_out.merge_cells(start_row=row, start_column=col * 2, end_row=row, end_column=col * 2 + 1)
+                ws_out.unmerge_cells(start_row=row - 2, start_column=1, end_row=row - 1 , end_column=1)
+                ws_out.unmerge_cells(start_row=row - 2, start_column=2, end_row=row - 1 , end_column=2)
+                ws_out.merge_cells(start_row=row - 2, start_column=1, end_row=row - 1 , end_column=2)
+            if ':' in str(ws_out.cell(row, 1).value):
+                ws_out.cell(row, 1).value = str(ws_out.cell(row, 1).value)[1:-3]
+
+        return wb_out
+
+
+    def bold_difference(self, old_wb, new_wb):
+        dif_cell_font = Font(bold=True)
+        old_ws = old_wb.active
+        new_ws = new_wb.active
+        if old_ws.max_row != new_ws.max_row:
+            print('files length dont match!')
+        limit = min(old_ws.max_row, new_ws.max_row)
+        for row in range(1, limit + 1):
+            for col in range(1, len(new_ws[row]) + 1):
+                if new_ws.cell(row, col).value != old_ws.cell(row, col).value:
+                    new_ws.cell(row, col).font = dif_cell_font
+
+
+    def bold_difference_v2(self, old_wb, new_wb):
+        dif_cell_font = Font(bold=True)
+        old_ws = old_wb.active
+        new_ws = new_wb.active
+        row = 1
+        while row < new_ws.max_row:
+            if 'Класс' in str(new_ws.cell(row, 1).value):
+                print(new_ws.cell(row, 1).value, row)
+                old_row = ''
+                for x in range(1, old_ws.max_row + 1):
+                    if old_ws.cell(x, 1).value == new_ws.cell(row, 1).value:
+                        old_row = x
+                        break
+                if not old_row:
+                    print('No matches')
+                    raise Exception
+                cur_new_row = row + 2
+                cur_old_row = old_row + 2
+                while not (new_ws.cell(cur_new_row, 1).value is None and new_ws.cell(cur_new_row + 1, 1).value is None):
+                    for col in range(1, len(new_ws[cur_new_row]) + 1):
+                        if new_ws.cell(cur_new_row, col).value != old_ws.cell(cur_old_row, col).value:
+                            new_ws.cell(cur_new_row, col).font = dif_cell_font
+                    cur_old_row += 1
+                    cur_new_row += 1
+                row = cur_new_row
             row += 1
-        pbar.update(1)
-    pbar.close()
-    # постобработка
-    # мешаем эксельному движку опять все превратить в дата/время
-    for row in range(1, ws_out.max_row + 1):
-        if '#' in str(ws_out.cell(row, 1).value):
-            for col in range(1,6):
-                ws_out.merge_cells(start_row=row, start_column=col * 2, end_row=row, end_column=col * 2 + 1)
-            ws_out.unmerge_cells(start_row=row - 2, start_column=1, end_row=row - 1 , end_column=1)
-            ws_out.unmerge_cells(start_row=row - 2, start_column=2, end_row=row - 1 , end_column=2)
-            ws_out.merge_cells(start_row=row - 2, start_column=1, end_row=row - 1 , end_column=2)
-        if ':' in str(ws_out.cell(row, 1).value):
-            ws_out.cell(row, 1).value = str(ws_out.cell(row, 1).value)[1:-3]
-
-    return wb_out
-
-
-def bold_difference(old_wb, new_wb):
-    dif_cell_font = Font(bold=True)
-    old_ws = old_wb.active
-    new_ws = new_wb.active
-    if old_ws.max_row != new_ws.max_row:
-        print('files length dont match!')
-    limit = min(old_ws.max_row, new_ws.max_row)
-    for row in range(1, limit + 1):
-        for col in range(1, len(new_ws[row]) + 1):
-            if new_ws.cell(row, col).value != old_ws.cell(row, col).value:
-                new_ws.cell(row, col).font = dif_cell_font
-
-
-def bold_difference_v2(old_wb, new_wb):
-    dif_cell_font = Font(bold=True)
-    old_ws = old_wb.active
-    new_ws = new_wb.active
-    row = 1
-    while row < new_ws.max_row:
-        if 'Класс' in new_ws.cell(row, 1).value:
-            old_row = ''
-            for x in range(1,old_ws.max_row + 1):
-                if old_ws.cell(x, 1).value == new_ws.cell(row, 1).value:
-                    old_row = x
-                    break
-            if not old_row:
-                print('No matches')
-                raise Exception
-            cur_new_row = row
-            cur_old_row = old_row
-            offset = 0
-
-        row += 1
 
 
 #
