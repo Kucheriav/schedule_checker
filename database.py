463b86d8f9e79955743a458f076de9944f957df9
Kucheriav <kuchborisovich@gmail.com>
2024-11-16 13:42:33 +0300

    a big step forward in creating my own schedule db from excell files/ the only thing that remains is the main schedule its self. see comments in creating_db_script.create_schedule_from_file

--- database.py
+++ database.py
@@ -8,12 +8,33 @@ engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-def init_db():
-    Base.metadata.create_all(bind=engine)
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+class Database:
+    def __init__(self):
+        self.session = SessionLocal()
+
+    def init_db(self):
+        Base.metadata.create_all(bind=engine)
+
+    def drop_db(self):
+        Base.metadata.drop_all(bind=engine)
+
+    def recreate_db(self):
+        self.drop_db()
+        print('db dropped!')
+        self.init_db()
+
+    def close(self):
+        self.session.close()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+    def add(self, obj):
+        self.session.add(obj)
+
+    def commit(self):
+        self.session.commit()
--- db_models.py
+++ db_models.py
@@ -2,10 +2,16 @@ from sqlalchemy import Column, Integer, String, ForeignKey, Table
 from sqlalchemy.orm import relationship
 from database import Base
 
+
 class Class(Base):
     __tablename__ = 'classes'
     id = Column(Integer, primary_key=True)
     name = Column(String, index=True, unique=True)
+    shift = Column(Integer)
+    quantity = Column(Integer)
+    lessons_min = Column(Integer)
+    lessons_max = Column(Integer)
+
 
 class Schedule(Base):
     __tablename__ = 'schedules'
@@ -17,17 +23,19 @@ class Schedule(Base):
     cabinet_id = Column(Integer, ForeignKey('cabinets.id'))
     teacher_id = Column(Integer, ForeignKey('teachers.id'))
 
-    class_ = relationship("Class", back_populates="schedules")
-    subject = relationship("Subject", back_populates="schedules")
-    cabinet = relationship("Cabinet", back_populates="schedules")
-    teacher = relationship("Teacher", back_populates="schedules")
+    # class_ = relationship("Class", back_populates="schedules")
+    # subject = relationship("Subject", back_populates="schedules")
+    # cabinet = relationship("Cabinet", back_populates="schedules")
+    # teacher = relationship("Teacher", back_populates="schedules")
 
 class Teacher(Base):
     __tablename__ = 'teachers'
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True, unique=True)
-    specializations = relationship("TeacherSpecialization", back_populates="teacher")
-    schedules = relationship("Schedule", back_populates="teacher")
+    surname = Column(String, index=True)
+    name_last_name = Column(String)
+    base_cabinet = Column(Integer, ForeignKey('cabinets.id'))
+    # specializations = relationship("TeacherSpecialization", back_populates="teacher")
+    # schedules = relationship("Schedule", back_populates="teacher")
 
 class TeacherSpecialization(Base):
     __tablename__ = 'teacher_specializations'
@@ -35,8 +43,8 @@ class TeacherSpecialization(Base):
     teacher_id = Column(Integer, ForeignKey('teachers.id'))
     subject_id = Column(Integer, ForeignKey('subjects.id'))
 
-    teacher = relationship("Teacher", back_populates="specializations")
-    subject = relationship("Subject", back_populates="teacher_specializations")
+    # teacher = relationship("Teacher", back_populates="specializations")
+    # subject = relationship("Subject", back_populates="teacher_specializations")
 
 class TeacherSchedule(Base):
     __tablename__ = 'teacher_schedules'
@@ -47,17 +55,20 @@ class TeacherSchedule(Base):
     class_id = Column(Integer, ForeignKey('classes.id'))
     subject_id = Column(Integer, ForeignKey('subjects.id'))
     cabinet_id = Column(Integer, ForeignKey('cabinets.id'))
-    teacher = relationship('Teacher', back_populates='schedules')
+
+    # teacher = relationship('Teacher', back_populates='schedules')
 
 class Subject(Base):
     __tablename__ = 'subjects'
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True, unique=True)
-    schedules = relationship("Schedule", back_populates="subject")
-    teacher_specializations = relationship("TeacherSpecialization", back_populates="subject")
+    # schedules = relationship("Schedule", back_populates="subject")
+    # teacher_specializations = relationship("TeacherSpecialization", back_populates="subject")
 
 class Cabinet(Base):
     __tablename__ = 'cabinets'
     id = Column(Integer, primary_key=True, index=True)
     number = Column(String, index=True, unique=True)
-    schedules = relationship("Schedule", back_populates="cabinet")
+    capacity = Column(Integer)
+
+    # schedules = relationship("Schedule", back_populates="cabinet")
